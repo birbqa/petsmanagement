42876bf6f0bb040a59c4bb6cba562d9185f198df
Alesia Mozghunova <alesia.mozghunova@gmail.com>
2024-03-18 21:50:08 +0200

    Readme added, new endpoit cats added

---
+++ README.md
@@ -0,0 +1,9 @@
+# Requirements 
+## MVP 
+- User should be able to receive information about cats who are ready for adoption. 
+- They should also be able to edit or delete information about cats. 
+
+## Release 1.0 
+- User should be able to filter this information. 
+- Only user with admin permissions should be able to edit this information. 
+
--- index.js
+++ index.js
@@ -4,10 +4,38 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+    if(req.url === "/") {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end("\"Cats live here!\"");
+    } else if (req.url === "/cats") {
+        if (req.method !== "GET") {
+            res.statusCode = 405;
+            res.end();
+            return;
+        }
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        const cats = [
+            {
+                "name": "Benya",
+                "age": 12,
+                "colour": "white-red",
+                "fur": "fluffy",
+            },
+            {
+                "name": "Monya",
+                "age": 6,
+                "colour": "white-black",
+                "fur": "fluffy",
+            },
+        ];
+        res.end(JSON.stringify(cats));
+    }
+    else {
+        res.statusCode = 404;
+        res.end();
+    }
 });
 
 server.listen(port, hostname, () => {
